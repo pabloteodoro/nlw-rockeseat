85eae5492eb1ef44d17ddd54ed75e7f7af685a78
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-09-10 23:48:23 -0300

    Update server.ts
    configure port for server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!!!');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.listen({
+    port: 4500,
+}).then(() => {
+    console.log('Server is running on port 4500');
+})

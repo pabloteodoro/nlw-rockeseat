4cdba082a956914a0d1e54a40c51779f53560cdf
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-09-11 18:59:25 -0300

    add tools
    add tools for fix error with zoid

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env'
+
 
 export default defineConfig({
-    
+
   schema: "./schema.ts",
+  out: "./.migrations",
   dialect: 'postgresql',
-  migrations: {
-    prefix: 'supabase'
-  }
+  dbCredentials: {
+    
+    url: env.DATABASE_URL,
+ 
+  },
 })
